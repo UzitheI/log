24f6f5cda3ca2adab0b9fcb0616b31577f74fecd
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-03-19 15:41:38 +0545

    init file

---
+++ backend/__init__.py
@@ -0,0 +1,8 @@
+from sqlalchemy import engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
+
