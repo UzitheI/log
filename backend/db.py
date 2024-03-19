6447b296c5c8ce8203fbb58382911fcc5026cc18
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-03-19 20:34:52 +0545

    get db

--- backend/db.py
+++ backend/db.py
@@ -1,1 +1,24 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+SQLALCHEMY_DATABSE_URL=os.getenv('DATABASE_URL')
+
+engine=create_engine(
+    SQLALCHEMY_DATABSE_URL,connect_args={'check_same_thread':False},
+    echo=True
+)
+sessionLocal=sessionmaker(autoflush=False,autocommit=False,bind=engine)
+
+Base=declarative_base()
+
+def getdb():
+    db=sessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
