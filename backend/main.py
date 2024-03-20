bfe9e89836534391f82ada054f5d8a3885027706
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-03-20 22:56:45 +0545

    main file

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,8 @@
 from fastapi import APIRouter, FastAPI, status, Depends
 
+app=FastAPI()
 
+from routes import user as user_route
+
+
+app.include_router(user_route.router,prefix='/user',tags=['user'])
