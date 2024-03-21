5e67444b8032ba0182b523c30f1bd568e79b8849
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-03-22 01:32:20 +0545

    db logic

--- backend/db.py
+++ backend/db.py
@@ -2,11 +2,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
-SQLALCHEMY_DATABSE_URL='mysql+pymysql://uziP:K@lapana77@localhost:3306/userName'
+SQLALCHEMY_DATABSE_URL= f"mysql+pymysql://uziP:Kalpana77!@localhost:3306/user_schema"
 
 engine=create_engine(
-    SQLALCHEMY_DATABSE_URL,connect_args={'check_same_thread':False},
+    SQLALCHEMY_DATABSE_URL,
     echo=True
 )
 sessionLocal=sessionmaker(autoflush=False,autocommit=False,bind=engine)
