f2df08cba5411cc374cfe005b9f640915dab57fb
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-03-22 01:30:09 +0545

    userModel changes

--- backend/models/user.py
+++ backend/models/user.py
@@ -6,17 +6,17 @@ from db import Base
 
 class common(Base):
     __tablename__='logtable'
-    log_id=Column(Integer, primary_key=True)
-    operation=Column(String)
-    createdBy=Column(String)
-    createdOn=Column(DateTime)
-    createdAt=Column(String)
+    log_id=Column(Integer,primary_key=True)
+    operation=Column(String(255))
+    createdBy=Column(String(344))
+    createdAt=Column(DateTime)
+    user_id=Column(Integer)
 
 class User(common):
     __tablename__='users'
     id=Column(Integer,primary_key=True)
-    username=Column(String,unique=True)
-    password=Column(String)
+    username=Column(String(233))
+    password=Column(String(234))
     log_id=Column(Integer, ForeignKey(common.log_id))
 
     
